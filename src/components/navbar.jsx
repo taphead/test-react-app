fc473683daec9cf21adf0c499ad228957a930a04
taphead <techne.trax@proton.me>
2025-05-26 11:01:25 +0600

    css fixes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="container">
+    <div className="navcontainer">
       <img src={logo} id="logo-image" alt="logo" />
       <div className="navbar">
         <Link to="/" className="nav-item">
