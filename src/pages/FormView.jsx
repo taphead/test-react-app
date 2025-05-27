86ae96790a8de9190a467cf8cb70b23dd6d4ecd6
taphead <techne.trax@proton.me>
2025-05-27 13:07:09 +0600

    added form validation

--- src/pages/FormView.jsx
+++ src/pages/FormView.jsx
@@ -11,6 +11,7 @@ export default function FormView() {
   const [userArray, setUserArray] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [editId, setEditId] = useState();
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     const storedUsers = localStorage.getItem("userList");
@@ -36,8 +37,68 @@ export default function FormView() {
     }
   }, [userArray, isLoaded]);
 
+  function isValidName(name) {
+    return name.length !== 0 && !/\d/.test(name);
+  }
+
+  function isValidWebsite(url) {
+    const pattern = new RegExp(
+      "^(https?:\\/\\/)?" +
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*))\\.)+" +
+        "[a-z]{2,}" +
+        "(\\:\\d+)?(\\/[-a-z\\d%@_.~+&:]*)*" +
+        "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" +
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    );
+    return pattern.test(url);
+  }
+
+  function isValidPhone(phone) {
+    const pattern = new RegExp(/^(?:\d[-]?){9}\d$/);
+    return pattern.test(phone);
+  }
+
   function handleFormSubmit(event) {
     event.preventDefault();
+
+    let newErrors = {};
+
+    if (!isValidName(name)) {
+      if (name.length === 0) {
+        newErrors.name = "Please enter a value in the name field";
+      } else {
+        newErrors.name = "Please enter a valid name";
+      }
+
+      setErrors(newErrors);
+      return;
+    }
+
+    if (!isValidName(city)) {
+      if (city.length === 0) {
+        newErrors.city = "Please enter a value in the ciy field";
+      } else {
+        newErrors.city = "Please enter a valid city name";
+      }
+
+      setErrors(newErrors);
+      return;
+    }
+
+    if (!isValidPhone(phone)) {
+      newErrors.phone = "Please enter a valid 10-digit phone number";
+      setErrors(newErrors);
+      return;
+    }
+    if (!isValidWebsite(website)) {
+      newErrors.website = "Please enter a valid URL";
+      setErrors(newErrors);
+      return;
+    }
+
+    setErrors({});
+
     let newUser = {
       id: userArray.length + 1,
       name: name,
@@ -108,43 +169,45 @@ export default function FormView() {
           type="text"
           id="name"
           name="name"
-          required
           value={name}
+          className={errors.name ? "input-error" : ""}
           onChange={(e) => setName(e.target.value)}
         />
         <br />
+        {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
         <label htmlFor="phone">Phone Number:</label>
         <input
-          type="tel"
+          type="text"
           id="phone"
           name="phone"
-          pattern="^[0-9\-]+$"
           placeholder="123-456-7890"
-          required
           value={phone}
+          className={errors.phone ? "input-error" : ""}
           onChange={(e) => setPhone(e.target.value)}
         />
         <br />
+        {errors.phone && <p style={{ color: "red" }}>{errors.phone}</p>}
         <label htmlFor="city">City:</label>
         <input
           type="text"
           id="city"
           name="city"
-          required
           value={city}
           onChange={(e) => setCity(e.target.value)}
         />
         <br />
+        {errors.city && <p style={{ color: "red" }}>{errors.city}</p>}
         <label htmlFor="website">Website:</label>
         <input
-          type="url"
+          type="text"
           id="website"
           name="website"
           placeholder="https://example.com"
-          required
           value={website}
+          className={errors.website ? "input-error" : ""}
           onChange={(e) => setWebSite(e.target.value)}
         />
+        {errors.website && <p style={{ color: "red" }}>{errors.website}</p>}
         <br />
         {editId && (
           <input
